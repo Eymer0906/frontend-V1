ac7057f4a7966d1387df375ab7914f5fe809b206
Eymer Aragon <eimer.aragonguardo@gmail.com>
2024-08-15 14:25:51 -0500

    my first commit

---
+++ funciones.js
@@ -0,0 +1,25 @@
+
+function redirectToHome() {
+    location.href = 'home.html';
+}
+function redirectToProducts() {
+    location.href = 'productos.html';
+}
+
+function redirectToOfertas(){
+    location.href='Ofertas.html';
+
+}
+
+function redirectToStores(){
+    location.href='tiendas.html';
+
+}
+function redirectToRegister(){
+location.href='registro.html';
+
+}
+function redirectToCar(){
+    location.href='carrito.html';
+}
+
